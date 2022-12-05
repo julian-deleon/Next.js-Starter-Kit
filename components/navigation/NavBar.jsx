2cbf716e39ea2bdf98718fa955113a42d960beec
Julian De Leon <jdeleon9@nait.ca>
2022-12-04 23:49:56 -0700

    styling to card text

--- components/navigation/NavBar.jsx
+++ components/navigation/NavBar.jsx
@@ -3,6 +3,7 @@ import MobileMenuButton from "./MobileMenuButton";
 import NavBarBranding from "./NavBarBranding";
 import NavBarLinks from "./NavBarLinks";
 
+
 function NavBar() {
     return (
         <nav className="p-2 shadow relative flex justify-between">    
